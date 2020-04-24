ff16a9ba8cfd0caaedce913c691d2ce45c264f4d
sushi-desu <hugemerge@gmail.com>
2020-04-24 14:37:24 +0900

    Update to calculate shipping type

--- src/components/simulator.tsx
+++ src/components/simulator.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react"
+import React, { useState, useCallback, useMemo } from "react"
 import { Grid, Divider, Heading } from "@chakra-ui/core"
 import { Goods, beddings } from "../models/goods"
 import { Area } from "../models/area"
@@ -7,38 +7,44 @@ import { Result } from "./result"
 import { GoodsCard } from "./goodsCard"
 
 export const Simulator = () => {
-  const [selected, setSelected] = useState<Goods[]>([])
+  const [goods, setGoods] = useState<Goods[]>([])
   const selectGoods = useCallback((goods: Goods) => () => {
-    setSelected(prev => [...prev, goods])
-  }, [setSelected])
+    setGoods(prev => [...prev, goods])
+  }, [setGoods])
 
   const [area, setArea] = useState<Area>("北海道")
   const selectArea = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
     const selected = event.target.value as Area
     setArea(selected)
-  }, [])
+  }, [setArea])
+
+  const shippingType = useMemo((): "布団袋小" | "布団袋大" => {
+    return goods.reduce((total, cur) => total + cur.point , 0) <= 3
+      ? "布団袋小"
+      : "布団袋大"
+  }, [goods])
 
   return (
     <div>
       <Heading m={4} size="lg" as="h1">料金シミュレーター</Heading>
 
-      <Cart selectedGoods={selected} setSelectGoods={setSelected} />
+      <Cart selectedGoods={goods} setSelectGoods={setGoods} />
 
       <Result
-        selectedGoods={selected}
+        selectedGoods={goods}
         selectedArea={area}
         setSelectedArea={selectArea}
-        type={"布団袋小"}
+        type={shippingType}
       />
 
       <Divider margin="30px 0" />
 
       <Grid gap={3} templateColumns="repeat(4, 1fr)">
-        {beddings.map((goods, i) => (
+        {beddings.map((v, i) => (
           <GoodsCard
             key={i}
-            goods={goods}
-            clickAction={selectGoods(goods)}
+            goods={v}
+            clickAction={selectGoods(v)}
             clickIcon="add"
             clickLabel="商品を追加"
           />

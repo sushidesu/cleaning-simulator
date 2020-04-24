6ba94924712323cd0c032310f7e4a8fea7eaf9c8
sushi-desu <hugemerge@gmail.com>
2020-04-24 16:03:14 +0900

    Fix font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react"
-import { ThemeProvider, CSSReset } from "@chakra-ui/core"
+import { theme, ThemeProvider, CSSReset } from "@chakra-ui/core"
 import { Simulator } from "./components/simulator"
+
+const fontfamily = '-apple-system, BlinkMacSystemFont, "Helvetica Neue", "Segoe UI", Hiragino Kaku Gothic ProN, "ヒラギノ角ゴ ProN W3", Meiryo, "メイリオ", "Noto Sans Japanese", sans-serif'
+
+const custom = {
+  ...theme,
+  fonts: {
+    body: fontfamily,
+    heading: fontfamily,
+    mono: "Menlo, monospace",
+  }
+}
 
 export default () => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={custom}>
       <CSSReset />
       <Simulator />
     </ThemeProvider>
